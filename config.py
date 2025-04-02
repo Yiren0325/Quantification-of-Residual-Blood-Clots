6d515c145fb35b248e7bb48a69aba92205f445b7
葉倚任 <38685782+Yiren0325@users.noreply.github.com>
2025-04-02 10:28:47 +0800

    Add files via upload
    update config

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import config
 dataSetting={
     'inputSize': 224,
     'classSize': 2,
-    'class_to_idx': {'10':0,'30':1}
+    'class_to_idx': {'10':0,'30':1},
+    'idx_to_class': {0:'10',1:'30'}
 }
 
 trainSetting={
